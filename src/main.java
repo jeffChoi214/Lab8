a9c26089b98545bddb81e4fa7406901d6350fabe
Jeff Choi <hyuch93@gmail.com>
2017-02-06 15:35:23 -0500

    added a couple methods to clean up main

--- src/main.java
+++ src/main.java
@@ -5,14 +5,62 @@ import java.util.Scanner;
 */
 
 public class main {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int arraySize;
-        int numBatters;
+    public static void runLoop(Scanner sc, int numBatters, double[] battingAvgs, double[] slugPcs) {
         int hitCounter = 0;
         int totalCounter = 0;
-        double battingAvg;
-        double slugPc;
+        int arraySize;
+
+        for (int i = 0; i < numBatters; ++i) {
+            System.out.print("Enter number of times at bat for batter " + i + ": ");
+            arraySize = sc.nextInt();
+            sc.nextLine();
+
+            int[] results = new int[arraySize];
+            int[][] results2 = new int[numBatters][arraySize];
+
+            System.out.println("");
+            System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
+
+            for (int j = 0; j < arraySize; ++j) {
+                while (true) { 
+                    System.out.print("Result for batter " + numBatters + " at-bat " + j + ": ");
+                    results2[i][j] = sc.nextInt();
+                    sc.nextLine();
+                   
+                    if (results2[i][j] >= 0 && results2[i][j] <= 4) {
+                        break;
+                    }
+                    else {
+                        System.out.println("Please enter a number between 0 and 4");
+                    } 
+
+                }
+
+                totalCounter += results2[i][j];
+
+                if (results2[i][j] > 0) {
+                    hitCounter += 1;
+                }
+            }
+            battingAvgs[i] = (double) hitCounter / (double) arraySize;
+            slugPcs[i] = (double) totalCounter / (double) arraySize;
+
+            hitCounter = 0;
+            totalCounter = 0;
+        }
+    }
+
+    public static void printStats(int numBatters, double[] battingAvgs, double[] slugPcs) {
+        for (int i = 0; i < numBatters; ++i) {
+            System.out.println("Batter " + i + " batting average: " + String.format("%.3f", battingAvgs[i]));
+            System.out.println("Batter " + i + " slugging Percentage: " + String.format("%.3f", slugPcs[i]));
+        }
+
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numBatters;
         char toContinue;
 
         System.out.println("Welcome to the Batting Average Calculator!");
@@ -27,71 +75,10 @@ public class main {
             double[] battingAvgs = new double[numBatters];
             double[] slugPcs = new double[numBatters];
 
-            for (int i = 0; i < numBatters; ++i) {
-                System.out.print("Enter number of times at bat for batter " + i + ": ");
-                arraySize = sc.nextInt();
-                sc.nextLine();
+            runLoop(sc, numBatters, battingAvgs, slugPcs);
+            printStats(numBatters, battingAvgs, slugPcs);
 
-                int[] results = new int[arraySize];
-                int[][] results2 = new int[numBatters][arraySize];
-
-                System.out.println("");
-                System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
-
-                for (int j = 0; j < arraySize; ++j) {
-                    while (true) { 
-                        System.out.print("Result for batter " + numBatters + " at-bat " + j + ": ");
-                        results2[i][j] = sc.nextInt();
-                        sc.nextLine();
-                       
-                        if (results2[i][j] >= 0 && results2[i][j] <= 4) {
-                            break;
-                        }
-                        else {
-                            System.out.println("Please enter a number between 0 and 4");
-                        } 
-
-                    }
-
-                    totalCounter += results2[i][j];
-
-                    if (results2[i][j] > 0) {
-                        hitCounter += 1;
-                    }
-                }
-                battingAvgs[i] = (double) hitCounter / (double) arraySize;
-                slugPcs[i] = (double) totalCounter / (double) arraySize;
-
-                hitCounter = 0;
-                totalCounter = 0;
-            }
-
-
-
-            /*
-            for (int i = 0; i < arraySize; ++i) {
-                System.out.print("Result for at-bat " + i + ": ");
-                results[i] = sc.nextInt();
-                sc.nextLine();
-                totalCounter += results[i];
-
-                if (results[i] > 0) {
-                    hitCounter += 1;
-                }
-            }
-
-            battingAvg = (double) hitCounter / (double) arraySize;
-            slugPc = (double) totalCounter / (double) arraySize;
-            System.out.println("Batting average: " + String.format("%.3f", battingAvg));
-            System.out.println("Slugging Percentage: " + String.format("%.3f", slugPc));
-            */ 
-
-            for (int q = 0; q < numBatters; ++q) {
-                System.out.println("Batter " + q + "batting average: " + String.format("%.3f", battingAvgs[q]));
-                System.out.println("Batter " + q + "slugging Percentage: " + String.format("%.3f", slugPcs[q]));
-            }
-            
-
+            // could put the below in a method if i used boolean variables for the while loops
             while (true) {
                 System.out.print("Another round? (y/n): ");
                 toContinue = sc.next().charAt(0);
@@ -104,9 +91,6 @@ public class main {
                 break;
             }
         }
-
-
-
     }
 
 }
